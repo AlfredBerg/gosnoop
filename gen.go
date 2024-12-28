894a1dedbbe619e1e981fb70a3dc7dbe79abd5a9
Alfred Berg <alfberg@kth.se>
2024-12-28 19:11:14 +0100

    generalize event chan

--- gen.go
+++
@@ -1,3 +0,0 @@
-package main
-
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -type event exec ./internal/ebpf/exec/exec.c
--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
-	"errors"
 	"fmt"
+	"gosnoop/internal/ebpf/exec"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
-	"github.com/cilium/ebpf/link"
-	"github.com/cilium/ebpf/ringbuf"
 	"github.com/cilium/ebpf/rlimit"
 )
 
@@ -21,51 +17,20 @@ func main() {
 
 	// Remove resource limits for kernels <5.11.
 	if err := rlimit.RemoveMemlock(); err != nil {
-		log.Fatal("Removing memlock:", err)
+		log.Fatal("failed emoving memlock, do you have root priveledges? err: ", err)
 	}
-	// Load the compiled eBPF ELF and load it into the kernel.
-	var objs execObjects
-	if err := loadExecObjects(&objs, nil); err != nil {
-		log.Fatal("Loading eBPF objects:", err)
-	}
-	defer objs.Close()
 
-	tp, err := link.Tracepoint("syscalls", "sys_enter_execve", objs.TraceExecve, nil)
+	e := exec.Exec{}
+	eventChan, err := e.ReceiveEvents()
 	if err != nil {
-		log.Fatalf("attatching tracepoint: %s", err)
+		log.Fatal("failed receiving exec events: ", err)
 	}
-	defer tp.Close()
-
-	rd, err := ringbuf.NewReader(objs.RingBuffer)
-	if err != nil {
-		log.Fatalf("opening ringbuf reader: %s", err)
-	}
-	defer rd.Close()
 
 	go func() {
 		<-stopper
-
-		if err := rd.Close(); err != nil {
-			log.Fatalf("closing ringbuf reader: %s", err)
-		}
 	}()
 
-	var event execEvent
-	for {
-		record, err := rd.Read()
-		if err != nil {
-			if errors.Is(err, ringbuf.ErrClosed) {
-				log.Println("received signal, exiting..")
-				return
-			}
-			log.Printf("reading from reader: %s", err)
-			continue
-		}
-
-		if err := binary.Read(bytes.NewBuffer(record.RawSample), binary.LittleEndian, &event); err != nil {
-			log.Printf("parsing ringbuf event: %s", err)
-			continue
-		}
-		fmt.Printf("Got event: comm %-64s, pid %-5d, exec path %-64s args %-64s\n", event.Command, event.Pid, event.Path, event.Argv)
+	for e := range eventChan {
+		fmt.Println(e.String())
 	}
 }
